73917384a10552070a7dedbcfd7f878f5a2ca184
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-30 11:42:52 +0300

    nested destructuring.

---
+++ objects.ts
@@ -0,0 +1,14 @@
+const profile = {
+  name: "jogn",
+  age: 26,
+  coords: { lat: 29, lng: 234 },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+
+const { age, name, coords }: { age: number; name: string; coords: object } = profile;
+
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
