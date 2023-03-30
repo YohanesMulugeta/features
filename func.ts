f03a55ea8be7df93a42999ebd98340064e44fbb4
Yohanes <yohanesmulugeta21@gmail.com>
2023-03-30 11:10:15 +0300

    never depend on the inference of a function returned value rather always anotate the arguments and returned value.

--- func.ts
+++ func.ts
@@ -1,3 +1,11 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+// Never depend on the the inference of TypeScript on infering what is returned from a function
+const subtract = (a: number, b: number) => {
+  // this thinks that we are not returning value
+  a - b;
+};
+
+const subtractAnot = (a: number, b: number): number => a - b;
